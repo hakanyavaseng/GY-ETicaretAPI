19c52e5cd8467e723823404cc72ba4f50cfd6c0f
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-04 22:06:03 +0300

    0402, onion arch design

---
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Program.cs
@@ -0,0 +1,29 @@
+using ETicaretAPI.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddPersistenceServices();
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
