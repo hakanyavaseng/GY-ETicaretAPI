34a05002e4ba633391cc5d53fa6f70011de5d313
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-04-28 17:46:40 +0300

    Multiple layout arch completed, routing mechanism implemented

--- ETicaretClient/src/app/admin/components/order/order.module.ts
+++ ETicaretClient/src/app/admin/components/order/order.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderComponent } from './order.component';
-
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -9,7 +9,10 @@ import { OrderComponent } from './order.component';
     OrderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: "", component: OrderComponent }
+    ])
   ]
 })
 export class OrderModule { }
