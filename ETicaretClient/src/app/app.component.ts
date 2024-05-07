5ce86579cc0356c83f69067451afb2d6f7080a24
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-05-07 20:40:35 +0300

    Jquery and bootstrap installed, UI navbar implemented, Jquery tested

--- ETicaretClient/src/app/app.component.ts
+++ ETicaretClient/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ETicaretClient';
 }
+
+$(document).ready(function() {
+  console.log('Hello from jQuery!');
+});
